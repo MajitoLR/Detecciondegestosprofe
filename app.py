c1a76af90c478ff0664964f74f5c42018815120d
MajitoLR <larreamariajose456@gmail.com>
2026-03-19 14:01:39 -0500

    Update app.py

--- app.py
+++ app.py
@@ -40,9 +40,9 @@ if img_file_buffer is not None:
     # run the inference
     prediction = model.predict(data)
     print(prediction)
-    if prediction[0][0]>0.5:
+    if prediction[0][0]>0.7:
       st.header('Majo '+str( prediction[0][0]) )
-    if prediction[0][1]>0.5:
+    if prediction[0][1]>0.7:
       st.header('Manu '+str( prediction[0][1]))
     #if prediction[0][2]>0.5:
     # st.header('Derecha, con Probabilidad: '+str( prediction[0][2]))
