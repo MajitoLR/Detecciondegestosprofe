bee66402076a9ec6f8d0a6d4c8d07d4e4e7d615c
MajitoLR <larreamariajose456@gmail.com>
2026-03-19 14:11:08 -0500

    Update app.py

--- app.py
+++ app.py
@@ -42,11 +42,10 @@ if img_file_buffer is not None:
     print(prediction)
     if prediction[0][0]>0.5:
       st.header('Majo '+str( prediction[0][0]) )
-        print("Majo")
         
     if prediction[0][1]>0.5:
       st.header('Manu '+str( prediction[0][1]))
-        print("Manu")
+       
     #if prediction[0][2]>0.5:
     # st.header('Derecha, con Probabilidad: '+str( prediction[0][2]))
 
